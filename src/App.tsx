edddcbf2622dfff8c781f378e91da2c131f379b6
刘超 <liuchao@aldt.com.cn>
2021-02-24 23:02:04 +0800

    ffdadasa

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    // console.log(isModalVisible);
+    console.log(isModalVisible);
   }, [isModalVisible]);
   const handleCancel = () => {
     setIsModalVisible(false);
