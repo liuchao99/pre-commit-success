e5deeab0bb8477f872403fa6d0bc18982dbf6746
刘超 <liuchao@aldt.com.cn>
2021-02-25 10:38:35 +0800

    feat(): djkasda

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
         Open Modal
       </Button>
       <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
+        <p>Some contents</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
       </Modal>
